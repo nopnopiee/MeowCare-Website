d00433d653cacb62507a4f33b2dc9ac61d85e6cc
nopnopiee <novifitriaindriarti@gmail.com>
2024-05-14 18:37:51 +0700

    melanjutkan home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,16 +11,20 @@ function Home () {
                 <a href="">
                     <button class="btn-utama">Selengkapnya</button>
                 </a>
-                <h3 class="pre-title"><span id="fade-in-typing"></span></h3>
-                <h1 class="home-name fade-in">NOVI FITRIA INDRIARTI</h1>
-                <p class="fade-in" id="fade-in-p">I am a student majoring in accounting information systems, Bina Sarana Informatika University, Tegal City Campus, batch 22. I have studied Zahir and Mayob for the digital accounting section, while my information systems section has learned the PHP programming language and Laravel 10 framework. I am currently participating in an independent study batch 6 of web development and UI &UX Design programs at Infinite Learning.</p>
-                <br />
-                <a href="#about">
-                    <button class="btn fade-in">My Resume</button>
-                </a>
-            </div>
+        </div>
+        <div class="keterangan-1">
+            <h1>Layanan Kami</h1>
+            <nav>
+                <ul>
+                    <li><a href="">Vaksin</a></li>
+                    <li><a href="">Petshop</a></li>
+                    <li><a href="">FAQ</a></li>
+                </ul>
+            </nav>
+        </div>
+        
         </section>
     );
 }
 
-export default Header;
+export default Home;
